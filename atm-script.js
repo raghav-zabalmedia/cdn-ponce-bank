340f8b403c4e05261025cf33d12c607f8f887fa0
raghav-zabalmedia <118424681+raghav-zabalmedia@users.noreply.github.com>
2024-02-14 12:08:10 +0530

    Update atm-script.js

--- atm-script.js
+++ atm-script.js
@@ -229,13 +229,13 @@ if (window.innerWidth < 479) {
     ) {
       $(".mapboxgl-ctrl-bottom-right")[0].style.setProperty(
         "bottom",
-        ($(".atm_content").height() - 50) + "px",
+        ($(".atm_content").height() - 40) + "px",
         "important"
       );
     } else {
       $(".mapboxgl-ctrl-bottom-right")[0].style.setProperty(
         "bottom",
-        ($(".atm_content").height() - 50) + "px",
+        ($(".atm_content").height() - 40) + "px",
         "important"
       );
     }
