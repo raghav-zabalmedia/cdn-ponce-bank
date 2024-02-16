bb8af001b80e26e069c9dd93a95457ca702f33a4
raghav-zabalmedia <118424681+raghav-zabalmedia@users.noreply.github.com>
2024-02-16 13:15:50 +0530

    Update atm-script.js

--- atm-script.js
+++ atm-script.js
@@ -1553,7 +1553,8 @@ async function getGeocodeFromAddress(searchText) {
     const params = {
         access_token: MAPBOX_ACCESS_TOKEN,
         types: 'postcode,place',
-        bbox: '-125.0,24.396308,-66.93457,49.384358'
+        //bbox: '-125.0,24.396308,-66.93457,49.384358'
+        country: "US"
     };
 
   return await fetch(url + '?' + new URLSearchParams(params))
